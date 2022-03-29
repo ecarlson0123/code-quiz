55079c654dfc38209e94b3dc48c0d3f4e2326415
MrVoze <ecarlson0123@yahoo.com>
2022-03-29 00:54:48 -0500

    working on high score storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,47 @@
 var timer = document.querySelector('#timer');
 var highscoreLink = document.querySelector('#highscore-link');
 var answers = document.querySelector('#answers');
-var correctness = document.querySelector('#correctness');
+var correctnessWrapper = document.querySelector('.correctness-wrapper');
 var subtext = document.querySelector('#subtext');
 var largeText = document.querySelector('#large-text');
+var textWrapper = document.querySelector('#text-wrapper');
 var timerCount = 60;
 var questionNumber= 0;
+var timeInterval;
+var highscores = []
 
-var questionsText= {"question1" : "what is the ...", question2: "blah blah blah"}
-var answersText = {"question1" : ["hi" , "hello", "wow", "yay"]}
 
+var questionsText= {
+ "question1" : "1.) Which of the following is true about variable naming conventions in JavaScript?",
+ "question2" : "2.) Which of the following is true about cookie handling in JavaScript?",
+ "question3" : "3.) Which built-in method removes the last element from an array and returns that element?",
+ "question4" : "4.) Which of the following function of Number object formats a number with a specific number of digits to the right of the decimal?",
+ "question5" : "5.) Which of the following function of String object returns the character at the specified index?",
+ "question6" : "6.) Which of the following function of String object returns the characters in a string beginning at the specified location through the specified number of characters?",
+ "question7" : "7.) Which of the following function of String object creates an HTML anchor that is used as a hypertext target?",
+ "question8" : "8.) Which of the following function of String object causes a string to be italic, as if it were in an <i> tag?",
+ "question9" : "9.) Which of the following function of Array object joins all elements of an array into a string?",
+ "question10" : "10.) Which of the following function of Array object adds one or more elements to the front of an array and returns the new length of the array?"
+};
 
-var answerHandler = function(event){
-    var targetEL= event.target;
-    if(targetEL.matches('#start-button')){
-        questionNumber++;
-        clearScreen();
-        createQuestion();
-    }
+/* THE FIRST ANSWER IN EACH DICTIONARY KEY IS THE CORRECT ANSWER!!! */
+var answersText = {
+ "question1" : ["Both.", "JavaScript variable names must begin with a letter or the underscore character.", "JavaScript variable names are case sensitive.", "Neither."] , 
+ "question2" : ["Both.", "JavaScript can manipulate cookies using the cookie property of the Document object.", "JavaScript can read, create, modify, and delete the cookie or cookies that apply to the current web page.", "Neither."],
+ "question3" : ["pop()", "last()", "get()", "None of these."],
+ "question4" : ["toFixed()", "toExponential()", "toPrecision()", "toLocaleString()"],
+ "question5" : ["charAt()", "charCodeAt()", "concat()", "indexOf()"],
+ "question6" : ["substr()", "slice()", "split()", "search()"],
+ "question7" : ["anchor()", "link()",  "blink()", "big()"],
+ "question8" : ["italics()", "fixed()",  "fontcolor()", "fontsize()"],
+ "question9" : ["join()", "concat()",  "pop()", "map()"],
+ "question10" : ["unshift()", "sort()",  "splice()", "toString()"]
 };
 
 
-
+ 
+ 
+ 
 var createQuestion = function(){
     clearScreen();
     largeText.textContent = questionsText["question"+questionNumber];
@@ -30,15 +50,12 @@ var createQuestion = function(){
     for(i=0; i<answerLength; i++){
         answerNumbers.push(i)
     };
-    console.log('answerlength: ' + answerLength);
     for( i=0; i<answerLength; i++){
-        console.log('answernum: ' + answerNumbers);
         randomSelect=answerNumbers[Math.floor(Math.random()*answerNumbers.length)];
-        console.log('random: ' + randomSelect);
         var newAnswer = document.createElement("li");
         newAnswer.className="answer"
         newAnswer.textContent= answersText["question"+questionNumber][randomSelect];
-        if(randomSelect==0){
+        if(randomSelect===0){
             newAnswer.id="correct"
         }
         else{
@@ -46,13 +63,12 @@ var createQuestion = function(){
         }
         answers.appendChild(newAnswer);
         for( x = 0; x < answerNumbers.length; x++){ 
-    
+            
             if ( answerNumbers[x] === randomSelect) { 
-        
+                
                 answerNumbers.splice(x, 1); 
             }
         };
-        console.log('answernum: ' + answerNumbers);
     };
 };
 
@@ -61,28 +77,114 @@ var clearScreen = function(){
         answers.removeChild(answers.firstChild);
     }
     subtext.textContent="";
+    
 };
 
-var respondToAnswer = function(){
-    questionNumber++;
+
+var timeFunction = function(string){
+    switch(string){
+        case "start":
+            timerCount=60;
+            timer.textContent=timerCount;
+            timeInterval= setInterval(function(){
+                if(timerCount>=1){
+                    timerCount--;
+                    timer.textContent=timerCount
+                } else if (timerCount <=0){
+                    clearInterval(timeInterval);
+                    timer.textContent=0;
+                    endGame();
+                }}, 1000);
+                break;
+        case "wrong":
+            if (timerCount>5){
+                timerCount-=5;
+            }
+            else if(timerCount<=5){
+                clearInterval(timeInterval);
+                timerCount=0;
+                timer.textContent=0
+                endGame();
+            };
+            break;
+        case "end":
+            clearInterval(timeInterval);
+            break;
+        };
 };
 
+var respondToAnswer = function(targetEL){
+    while(correctnessWrapper.firstChild){
+        correctnessWrapper.removeChild(correctnessWrapper.firstChild);
+    };
+    if(targetEL.matches('#correct')){
+        var correctness = document.createElement("div");
+        correctness.id="correctness";
+        correctness.textContent="CORRECT!"
+        correctnessWrapper.appendChild(correctness);
+    }
+    else  if(targetEL.matches('#wrong')){
+        var correctness = document.createElement("div");
+        correctness.id="correctness";
+        correctness.textContent="WRONG!"
+        correctnessWrapper.appendChild(correctness);
+        timeFunction("wrong");
+        
+    };
+    if (questionNumber===(Object.keys(questionsText).length)){
+        timeFunction("end");
+        endGame();
+    }
+    else{
+        questionNumber++;
+        createQuestion();
+        console.log(questionNumber);
+    };
+};
+            
 var showHighscores = function(){
-
+    
 };
 
 var addHighscore = function(){
-
+    var initials = document.querySelector("input[id='#userInitials']").value
+    var userScore = [initials , timerCount];
+    if(highscores.length==0){
+        highscores.push(userScore);
+    }
+    /* WORKING ON HIGHSCORE STORAGE */
+    else{
+        /* var tooLow = false */
+        for(i=0; i<highscores.length; i++){
+            if(timerCount>highscores[i][1]){
+                highscores.splice(i,0);
+                break;
+            }
+        };
+    };
 };
 
 var startGame = function(){
-
+    
 };
 
 var endGame = function(){
-    questionNumber=0;
+    while(correctnessWrapper.firstChild){
+        correctnessWrapper.removeChild(correctnessWrapper.firstChild);
+    };
+    clearScreen();
+    largeText.textContent="GAME OVER!";
+    subtext.textContent = "Your score: " + timerCount;
+    var userInitials= document.createElement('input');
+    userInitials.id="userInitials";
+    var submitEl = document.createElement('li');
+    submitEl.id = "submit-button";
+    submitEl.textContent = "SUBMIT"
+    textWrapper.appendChild(userInitials);
+    answers.appendChild(submitEl);
+    questionNumber=0; 
 };
-
+            
 var createStartScreen = function(){
     highscoreLink.textContent = 'View Highscores';
     largeText.textContent = 'Welcome to the Coding Quiz!';
@@ -92,6 +194,23 @@ var createStartScreen = function(){
     startButton.textContent = "Start Game!"
     answers.appendChild(startButton);
 };
+            
+var answerHandler = function(event){
+    var targetEL= event.target;
+    if(targetEL.matches('#start-button')){
+        questionNumber++;
+        clearScreen();
+        createQuestion();
+        timeFunction("start");
+    }
+    else if(targetEL.matches('#correct') || targetEL.matches('#wrong')){
+        respondToAnswer(targetEL);
+    } 
+    else if(targetEL.matches('#submit-button')){
+
+    }
+};
+
 
 answers.addEventListener("click", answerHandler);
 
